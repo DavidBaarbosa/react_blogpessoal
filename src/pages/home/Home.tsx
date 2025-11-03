30cf480d5f144d5ff7e9838824059fd72d6772b6
davidbaarbosa <davidbaarbosa18@gmail.com>
2025-11-03 00:36:31 -0300

    Atualizações

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import ListaPostagens from '../../components/postagem/listapostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagem/modalpostagem/ModalPostagem'
 
 function Home() {
   return (
@@ -14,10 +15,7 @@ function Home() {
             </p>
 
             <div className='flex justify-around gap-4'>
-              <div className='rounded text-white
-                              border-white border-solid border-2 py-2 px-4'>
-                Nova Postagem
-              </div>
+               <ModalPostagem />
             </div>
           </div>
 
@@ -30,6 +28,7 @@ function Home() {
           </div>
         </div>
       </div>
+       <ListaPostagens />
     </>
   )
 }
