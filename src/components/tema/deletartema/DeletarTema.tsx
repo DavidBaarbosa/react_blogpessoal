b1aeff1fbfbd17af29f57ebeeefd15851b6f934b
davidbaarbosa <davidbaarbosa18@gmail.com>
2025-11-08 16:48:42 -0300

    .

--- src/components/tema/deletartema/DeletarTema.tsx
+++ src/components/tema/deletartema/DeletarTema.tsx
@@ -1,89 +1,64 @@
 import { useContext, useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { ClipLoader } from 'react-spinners';
-import { AuthContext } from '../../../contexts/AuthContext';
 import type Tema from '../../../models/Tema';
+import { AuthContext } from '../../../contexts/AuthContext';
 import { buscar, deletar } from '../../../services/Service';
 
 function DeletarTema() {
 
-const navigate = useNavigate()
+  // const [tema, setTema] = useState<Tema>({} as Tema)
+  const [tema, setTema] = useState<Tema>({
+    id: 0,
+    descricao: ''
+  })
 
-const [tema, setTema] = useState<Tema>({} as Tema)
+  const navigate = useNavigate()
 
-const [isLoading, setIsLoading] = useState<boolean>(false)
+  const {id} = useParams<{id: string}>()
 
-const { usuario, handleLogout } = useContext(AuthContext)
-const token = usuario.token
+  const {usuario} = useContext(AuthContext)
 
-const { id } = useParams<{ id: string }>()
-
-async function buscarPorId(id: string) {
-  try {
-    await buscar(`/temas/${id}`, setTema, {
-      headers: {
-        'Authorization': token
-      }
-    })
-  } catch (error: any) {
-    if (error.toString().includes('401')) {
-      handleLogout()
-    }
-  }
-}
-
-useEffect(() => {
-  if (token === '') {
-    alert('Você precisa estar logado')
-    navigate('/')
-  }
-}, [token])
-
-useEffect(() => {
-  if (id !== undefined) {
-    buscarPorId(id)
-  }
-}, [id])
-
-async function deletarTema() {
-  setIsLoading(true)
-
-  try {
-    await deletar(`/temas/${id}`, {
-      headers: {
-        'Authorization': token
-      }
-    })
-
-    alert('Tema deletado com sucesso')
-
-  } catch (error: any) {
-    if (error.toString().includes('401')) {
-      handleLogout()
-    } else {
-      alert('Erro ao deletar o tema.')
+  // dar um get por ID no backend
+  async function getTemaPorId(id: string) {
+    try {
+      await buscar(`/temas/${id}`, setTema, {
+        headers: {Authorization: usuario.token}
+      })
+    } catch(error) {
+      alert('deu ruim')
     }
   }
 
-  setIsLoading(false)
-  retornar()
-}
+  useEffect(() => {
+    if (id !== undefined) {
+      getTemaPorId(id)
+    }
+  }, [id])
 
-function retornar() {
-  navigate("/temas")
-}
+  // deletar o tema, se o usuario confirmar
+  async function deletarTema() {
+    try {
+      await deletar(`/temas/${id}`, {
+        headers: {Authorization: usuario.token}
+      })
+      alert('Press F for the tema')
+      navigate('/temas')
+    } catch (error) {
+      alert('Foi não...')
+    }
+  }
 
+  function retornar(){
+    navigate('/temas')
+  }
 
   return (
     <div className='flex flex-col items-center gap-4'>
       <h1 className='text-5xl font-bold text-stone-800'>Deletar tema</h1>
-      <p className='text-lg font-semibold'>
-        Você tem certeza de que deseja apagar o tema a seguir?</p>
+      <p className='text-lg font-semibold'>Tem certeza de que deseja sumir de vez com isso?</p>
       <div className="border-2 rounded-xl overflow-auto w-1/3">
-        <header
-        className="bg-stone-900 text-white text-xl font-bold px-4 py-2">
-          Tema
-          </header>
+        <div className="bg-stone-900 text-white text-xl font-bold px-4 py-2">
+          Titulo
         </div>
         <div className="bg-slate-200 px-4 py-6 text-lg font-semibold">{tema.descricao}</div>
         <div className="flex">
@@ -98,6 +73,7 @@ function retornar() {
           </button>
         </div>
       </div>
+    </div>
   );
 }
 
